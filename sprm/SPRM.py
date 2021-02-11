d150a03e536862db420c250a82c0f5e2a9fcbb02
Matt Ruffalo <matt.ruffalo@gmail.com>
2021-02-11 15:12:13 -0500

    SPRM.main: fix mixup of type annotation vs. default argument

--- sprm/SPRM.py
+++ sprm/SPRM.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from .SPRM_pkg import *
 from .outlinePCA import getparametricoutline, getcellshapefeatures, pca_cluster_shape,  pca_recon, bin_pca
 
@@ -23,9 +25,9 @@ DEFAULT_TEMP_DIRECTORY = Path('temp')
 def main(
         img_dir: Path,
         mask_dir: Path,
-        output_dir: DEFAULT_OUTPUT_PATH,
-        options_path: DEFAULT_OUTPUT_PATH,
-        optional_img_dir=None
+        output_dir: Path = DEFAULT_OUTPUT_PATH,
+        options_path: Path = DEFAULT_OPTIONS_FILE,
+        optional_img_dir: Optional[Path] = None,
 ):
     # get_imgs sorts to ensure the order of images and masks matches
     img_files = get_paths(img_dir)
