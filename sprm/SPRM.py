9cc960d4236e02036c4c6c565823c912b850e85d
Matt Ruffalo <matt.ruffalo@gmail.com>
2021-02-17 14:34:12 -0500

    Move argparse usage to sprm.SPRM.argparse_wrapper
    setup.py uses this entry point to install a command-line executable.

--- sprm/SPRM.py
+++ sprm/SPRM.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from typing import Optional
 
 from .SPRM_pkg import *
@@ -212,3 +213,18 @@ def main(
 
     # recluster features
     # recluster(output_dir, im, options)
+
+
+def argparse_wrapper():
+    p = ArgumentParser()
+    p.add_argument('img_dir', type=Path)
+    p.add_argument('mask_dir', type=Path)
+    p.add_argument('--output-dir', type=Path, default=DEFAULT_OUTPUT_PATH)
+    p.add_argument('--options-file', type=Path, default=DEFAULT_OPTIONS_FILE)
+    p.add_argument('optional_img_dir', type=Path, nargs='?', default=False)
+    argss = p.parse_args()
+
+    if argss.optional_img_dir:
+        main(argss.img_dir, argss.mask_dir, argss.output_dir, argss.options_file, argss.optional_img_dir)
+    else:
+        main(argss.img_dir, argss.mask_dir, argss.output_dir, argss.options_file)
