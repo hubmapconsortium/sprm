ce984a8b70d939ea886395b76ac547bc7b4ae3bb
hrch3n <chr_3742@hotmail.com>
2021-06-03 20:37:33 -0400

    update

--- sprm/single_method_eval.py
+++ sprm/single_method_eval.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from skimage.morphology import disk
 from skimage.morphology import closing
 from skimage.morphology import diameter_closing
-
+from .get_matched_masks import *
 """
 
 Companion to SPRM.py
@@ -38,20 +38,28 @@ def foreground_separation(img):
 	img_sep = img > threshold
 	img_sep = img_sep.astype(int)
 	img_sep = closing(img_sep, disk(3))
-	img_sep = -img_sep + 1
-	img_sep = diameter_closing(img_sep, 5)
-	labels = measure.label(img_sep, background=0, connectivity=2)
-	label_num = np.unique(labels)
-	for i in label_num:
-		label_loc = np.where(labels == i)
-		if i == 0:
-			labels[label_loc] = 0
-		elif len(label_loc[0]) > 5000:
-			labels[label_loc] = -1
-		else:
-			labels[label_loc] = 0
-	labels = labels + 1
-	return labels
+	# img_sep = -img_sep + 1
+	# img_sep = diameter_closing(img_sep, 5)
+	#
+	# labels = measure.label(img_sep, background=0, connectivity=2)
+	#
+	# label_num = np.unique(labels)
+	# label_coords = get_indices_sparse(labels)
+	# # label_coords = list(map(lambda x: np.array(x).T, label_coords))
+	# for i in label_num:
+	# 	label_loc = label_coords[i]
+	#
+	# 	if i == 0:
+	# 		labels[label_loc] = 0
+	# 	elif len(label_loc) > 5000:
+	# 		labels[label_loc] = -1
+	# 	else:
+	# 		labels[label_loc] = 0
+	# labels = labels + 1
+	# plt.imshow(labels)
+	# plt.show()
+	
+	return img_sep
 
 
 def uniformity_CV(loc, channels):
@@ -62,7 +70,7 @@ def uniformity_CV(loc, channels):
 		channel = channel / np.mean(channel)
 		background_intensity = channel[tuple(loc.T)]
 		CV.append(np.std(background_intensity))
-	return np.average(CV)
+	return 1 / np.average(CV)
 
 
 def uniformity_fraction(loc, channels):
@@ -119,13 +127,13 @@ def weighted_by_cluster(vector, labels):
 def cell_uniformity(mask, channels):
 	cell_index = np.unique(mask)
 	n = len(channels)
-	cell_coord = get_indices_sparse(mask)
+	cell_coord = get_indices_sparse(mask)[1:]
 	for i in range(n):
 		# channel = imread(os.path.join(data_dir, 'R001_X001_Y001.ome-' + str(i) + '.tif'))
 		channel = channels[i]
 		channel = channel / np.mean(channel)
 		cell_intensity = []
-		for j in range(1, len(cell_index)):
+		for j in range(len(cell_index)-1):
 			cell_intensity.append(np.sum(channel[tuple(cell_coord[j])]))
 		if i == 0:
 			feature_matrix = np.array(cell_intensity)
@@ -152,7 +160,7 @@ def cell_uniformity(mask, channels):
 			cluster_feature_matrix = feature_matrix[np.where(labels == i)[0], :]
 			CV_current.append(cell_uniformity_CV(cluster_feature_matrix))
 			fraction_current.append(cell_uniformity_fraction(cluster_feature_matrix))
-		CV.append(weighted_by_cluster(CV_current, labels))
+		CV.append(1 / weighted_by_cluster(CV_current, labels))
 		fraction.append(weighted_by_cluster(fraction_current, labels))
 	return CV, fraction, silhouette
 
@@ -219,95 +227,109 @@ def get_mask(cell_list, mask_shape):
 	return mask
 
 
-def get_matched_mask(whole_cell_mask, nuclear_mask):
-	
-
-	cell_coords = get_indices_sparse(whole_cell_mask)[1:]
-	nucleus_coords = get_indices_sparse(nuclear_mask)[1:]
-	
-	cell_coords = list(map(lambda x: np.array(x).T, cell_coords))
-	nucleus_coords = list(map(lambda x: np.array(x).T, nucleus_coords))
-	
-	cell_matched_index_list = []
-	nucleus_matched_index_list = []
-	cell_matched_list = []
-	nucleus_matched_list = []
-	
-	for i in range(len(cell_coords)):
-		if (i not in cell_matched_index_list) and len(cell_coords[i]) != 0:
-			current_cell_coords = cell_coords[i]
-			nuclear_search_num = np.unique(list(map(lambda x: nuclear_mask[tuple(x)], current_cell_coords))) - 1
-			for j in nuclear_search_num:
-				if j != -1 and j != 65535 and (j not in nucleus_matched_index_list):
-					whole_cell, nucleus = get_matched_cells(cell_coords[i], nucleus_coords[j])
-					if type(whole_cell) != bool:
-						cell_matched_list.append(whole_cell)
-						nucleus_matched_list.append(nucleus)
-						cell_matched_index_list.append(i)
-						nucleus_matched_index_list.append(j)
-	
-	cell_matched_mask = get_mask(cell_matched_list, whole_cell_mask.shape)
-	nuclear_matched_mask = get_mask(nucleus_matched_list, whole_cell_mask.shape)
-	return cell_matched_mask, nuclear_matched_mask
-
-
-def single_method_eval(img, mask, result_dir):
+def single_method_eval(img, mask, mask_dir, result_dir):
 	print('Calculating single-method metrics...')
-	
+	try:
+		os.makedirs(join(result_dir, 'single_method_metric'))
+	except:
+		pass
 	# get best z slice for future use
 	bestz = mask.bestz
 	
-	# separate image foreground background
-	img_binary = foreground_separation(np.sum(np.squeeze(img.data[0, 0, :, bestz, :, :], axis=0), axis=0))
-
-	# set mask channel names
-	channel_names = ['cells', 'nucleus', 'cell_membrane', 'nuclear_membrane']
+	# get compartment masks
+	matched_mask = np.squeeze(mask.data[0, 0, :, bestz, :, :], axis=0)
+	cell_matched_mask = matched_mask[0]
+	nuclear_matched_mask = matched_mask[1]
+	cell_membrane_mask = matched_mask[2]
+	nuclear_membrane_mask = matched_mask[3]
+	no_mem_cell_matched_mask = cell_matched_mask - cell_membrane_mask
+	no_mem_nuclear_matched_mask = nuclear_matched_mask - nuclear_membrane_mask
+	cytoplasm_mask = (no_mem_cell_matched_mask - nuclear_matched_mask).clip(min=0)
 	
-	for channel in range(mask.data.shape[2]):
+	metric_mask = np.expand_dims(cell_metric_mask, 0)
+	metric_mask = np.vstack((metric_mask, np.expand_dims(cell_membrane_mask, 0)))
+	metric_mask = np.vstack((metric_mask, np.expand_dims(cytoplasm_mask, 0)))
+	metric_mask = np.vstack((metric_mask, np.expand_dims(nuclear_membrane_mask, 0)))
+	metric_mask = np.vstack((metric_mask, np.expand_dims(no_mem_nuclear_matched_mask, 0)))
+	
+	# separate image foreground background
+	img_bi_dir = join(result_dir, 'single_method_metric', 'img_binary.txt')
+	if not os.path.exists(img_bi_dir):
+		img_binary = foreground_separation(np.sum(np.squeeze(img.data[0, 0, :, bestz, :, :], axis=0), axis=0))
+		np.savetxt(img_bi_dir, img_binary)
+		plt.imshow(img_binary)
+		plt.savefig(img_bi_dir[:-4] + '.png')
+	else:
+		img_binary = np.loadtxt(img_bi_dir)
+	# set mask channel names
+	channel_names = ['matched_cells', 'cell_membrane', 'cytoplasm', 'nuclear_membrane', 'nucleus_no_mem']
+	
+	metrics = {}
+	for channel in range(metric_mask.shape[2]):
 		
 		# set output directory for current mask channel
 		channel_dir = join(result_dir, 'single_method_metric', channel_names[channel])
 		if not os.path.exists(channel_dir):
 			os.makedirs(channel_dir)
-			
-		# calculate mismatch metric if whole-cell mask
-		if channel == 0:
-			whole_cell_mask = np.squeeze(mask.data[0, 0, 0, bestz, :, :], axis=0)
-			nuclear_mask = np.squeeze(mask.data[0, 0, 1, bestz, :, :], axis=0)
-			cell_matched_mask, nuclear_matched_mask = get_matched_mask(whole_cell_mask, nuclear_mask)
-			whole_cell_mask_binary = np.sign(whole_cell_mask)
-			nuclear_mask_binary = np.sign(nuclear_mask)
-			cell_matched_mask_binary = np.sign(cell_matched_mask)
-			nuclear_matched_mask_binary = np.sign(nuclear_matched_mask)
-			total_area = np.sum(np.sign(whole_cell_mask_binary + nuclear_mask_binary))
-			mismatched_area = np.sum(np.sign((nuclear_mask_binary - nuclear_matched_mask_binary) + (whole_cell_mask_binary - cell_matched_mask_binary)))
-			mismatched_fraction = mismatched_area / total_area
-			np.savetxt(join(channel_dir, 'compartments_mismatched_fraction.txt'), [mismatched_fraction])
-		
-		# get current mask
-		current_mask = np.squeeze(mask.data[0, 0, channel, bestz, :, :], axis=0)
-		# get current binary mask
+		current_mask = metric_mask[channel]
 		mask_binary = np.sign(current_mask)
 		
-		# calculate number of cell
-		cell_num = len(np.unique(current_mask)) - 1
+		if channel == 0:
+			# calculate mismatch metric if whole-cell mask
+			# mismatched_fraction_dir = mask_dir[:-8] + '_compartments_mismatched_fraction.txt'
+			# 	if not os.path.exists(mismatched_fraction_dir):
+			# 		whole_cell_mask = np.squeeze(mask.data[0, 0, 0, bestz, :, :], axis=0)
+			# 		nuclear_mask = np.squeeze(mask.data[0, 0, 1, bestz, :, :], axis=0)
+			# 		cell_matched_mask, nuclear_matched_mask = get_matched_mask(whole_cell_mask, nuclear_mask)
+			# 		whole_cell_mask_binary = np.sign(whole_cell_mask)
+			# 		nuclear_mask_binary = np.sign(nuclear_mask)
+			# 		cell_matched_mask_binary = np.sign(cell_matched_mask)
+			# 		nuclear_matched_mask_binary = np.sign(nuclear_matched_mask)
+			# 		total_area = np.sum(np.sign(whole_cell_mask_binary + nuclear_mask_binary))
+			# 		mismatched_area = np.sum(np.sign((nuclear_mask_binary - nuclear_matched_mask_binary) + (whole_cell_mask_binary - cell_matched_mask_binary)))
+			# 		mismatched_fraction = mismatched_area / total_area
+			# 		np.savetxt(join(channel_dir, 'compartments_mismatched_fraction.txt'), [mismatched_fraction])
+			# 	else:
+			# 		os.system('mv ' + mismatched_fraction_dir + ' ' + join(channel_dir, 'compartments_mismatched_fraction.txt'))
+			# 	np.savetxt(join(channel_dir, 'compartments_mismatched_fraction.txt'), [0])  # temp
+			
+			# get pixel size in micron
 		
-		# get fraction of image occupied by the mask
-		mask_fraction = 1 - (len(np.where(current_mask == 0)[0]) / (current_mask.shape[0] * current_mask.shape[1]))
+			start_ind = img.img.metadata.to_xml().find('PhysicalSizeX=') + len('PhysicalSizeX=') + 1
+			pixel_size = float(img.img.metadata.to_xml()[start_ind:start_ind+5]) ** 2
+			pixel_num = mask_binary.shape[0] * mask_binary.shape[1]
+			micron_num = pixel_size * pixel_num
+			
+			# calculate number of cell per 1000 micron^2
+			cell_num = len(np.unique(current_mask)) - 1
+			cell_num_normalized = cell_num / micron_num * 1000
+			
+			# get fraction of image occupied by the mask
+			mask_fraction = 1 - (len(np.where(current_mask == 0)[0]) / (current_mask.shape[0] * current_mask.shape[1]))
+			
+			# get coverage metrics
+			foreground_fraction, background_fraction, _ = fraction(img_binary, mask_binary)
+			# np.savetxt(join(channel_dir, 'cell_basic.txt'), [cell_num_normalized, mask_fraction, foreground_fraction, background_fraction, mask_foreground_fraction])
+			mismatched_fraction = 0  # temp solution, wait for Vasyl's update
+			metrics{channel[i]}{'NumberOfCellPerMicron'} = cell_num_normalized
+			
+		else:
+			_, _, mask_foreground_fraction = fraction(img_binary, mask_binary)
+			img_channels = np.squeeze(img.data[0, 0, :, bestz, :, :], axis=0)
+			# get background and foreground uniformity
+			foreground_CV, foreground_PCA = foreground_uniformity(img_binary, mask_binary, img_channels)
+			background_CV, background_PCA = background_uniformity(img_binary, mask_binary, img_channels)
+			# np.savetxt(join(channel_dir, 'foreground_uniformity.txt'), [foreground_CV, foreground_PCA])
+			# np.savetxt(join(channel_dir, 'background_uniformity.txt'), [background_CV, background_PCA])
+			
+			# get cell uniformity
+			cell_CV, cell_fraction, cell_silhouette = cell_uniformity(current_mask, img_channels)
+			avg_cell_CV = np.average(cell_CV)
+			avg_cell_fraction = np.average(cell_fraction)
+			avg_cell_silhouette = np.average(cell_silhouette)
+			
+			# np.savetxt(join(channel_dir, 'cell_uniformity.txt'), [cell_CV, cell_fraction, cell_silhouette])
 		
-		# get coverage metrics
-		foreground_fraction, background_fraction, mask_foreground_fraction = fraction(img_binary, mask_binary)
-		np.savetxt(join(channel_dir, 'cell_basic.txt'), [cell_num, mask_fraction, foreground_fraction, background_fraction, mask_foreground_fraction])
-		
-		img_channels = np.squeeze(img.data[0, 0, :, bestz, :, :], axis=0)
-		# get background and foreground uniformity
-		foreground_CV, foreground_PCA = foreground_uniformity(img_binary, mask_binary, img_channels)
-		background_CV, background_PCA = background_uniformity(img_binary, mask_binary, img_channels)
-		np.savetxt(join(channel_dir, 'foreground_uniformity.txt'), [foreground_CV, foreground_PCA])
-		np.savetxt(join(channel_dir, 'background_uniformity.txt'), [background_CV, background_PCA])
-		
-		# get cell uniformity
-		cell_CV, cell_fraction, cell_silhouette = cell_uniformity(current_mask, img_channels)
-		np.savetxt(join(channel_dir, 'cell_uniformity.txt'), [cell_CV, cell_fraction, cell_silhouette])
+	
 
 
