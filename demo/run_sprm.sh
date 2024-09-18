5e251f2f18050e8c5805c50c6a528cb0fe078aaf
Robert F. Murphy <murphy@cmu.edu>
2024-09-18 10:48:29 -0400

    include example paths in demo; update README

--- demo/run_sprm.sh
+++ demo/run_sprm.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# this script assumes it is run from the demo folder
+
 # Path to the sprm executable
 SPRM_PATH="../SPRM.py"
 
-# Command to run sprm
-python -u $SPRM_PATH --img-dir img --mask-dir mask --output-dir sprm_demo_outputs > sprm_demo_outputs/sprm_demo_outputs.log
+demodir=`pwd`
+#echo 'Running from '$demodir
+if [ ! -e "sprm_demo_outputs" ]; then
+    mkdir "sprm_demo_outputs"
+fi
 
-# print out finish when done
-echo "Finish running sprm"
+#python -u $SPRM_PATH --img-dir img --mask-dir mask --output-dir sprm_demo_outputs > sprm_demo_outputs/
+python -u $SPRM_PATH --img-dir $demodir/image_demo.ome.tiff --mask-dir $demodir/mask_demo.ome.tiff --output-dir $demodir/sprm_demo_outputs > $demodir/sprm_demo_outputs/sprm_demo_outputs.log
+echo "Finished running sprm"
