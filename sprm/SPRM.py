39ade76847bbe0abff2302ea0e6e1ce45e905c8f
tCeZ <tedzhang1995@gmail.com>
2024-12-09 19:17:02 -0500

    add feature for multi cell types. Release of SPRM v1.5.0

--- sprm/SPRM.py
+++ sprm/SPRM.py
@@ -413,52 +413,52 @@ def main(
     stime = time.monotonic() if options.get("debug") else None
 
     ### LOCAL TESTING ###
-    # for i in range(len(img_files)):
-    #     im, mask, cc, segm = analysis(
-    #         img_files[i],
-    #         mask_files[i],
-    #         opt_img_files[0],
-    #         output_dir,
-    #         options,
-    #         celltype_files[i],
-    #     )
-    #
-    #     im_list.append(im)
-    #     mask_list.append(mask)
-    #     cell_total.append(cc)
-    #     seg_metric_list.append(segm)
+    for i in range(len(img_files)):
+        im, mask, cc, segm = analysis(
+            img_files[i],
+            mask_files[i],
+            opt_img_files[0],
+            output_dir,
+            options,
+            cell_types_by_image[i],
+        )
+
+        im_list.append(im)
+        mask_list.append(mask)
+        cell_total.append(cc)
+        seg_metric_list.append(segm)
     #### END LOCAL TESTING ###
 
     ### CWL RUNS ###
-    use_subprocess_isolation = len(img_files) > 1 and not options.get("debug")
-    executor = ProcessPoolExecutor if use_subprocess_isolation else ThreadPoolExecutor
-    print("Using", processes, "worker(s) with executor", executor.__name__)
-    with executor(max_workers=processes) as executor:
-        futures = []
-        for img_file, mask_file, opt_img_file, cell_types in zip(
-            img_files, mask_files, opt_img_files, cell_types_by_image
-        ):
-            futures.append(
-                executor.submit(
-                    analysis,
-                    img_file,
-                    mask_file,
-                    opt_img_file,
-                    output_dir,
-                    options,
-                    cell_types,
-                )
-            )
-
-        for future in futures:
-            maybe_result = future.result()
-            if maybe_result is not None:
-                im, mask, cell_count, seg_metrics = maybe_result
-
-                im_list.append(im)
-                mask_list.append(mask)
-                cell_total.append(cell_count)
-                seg_metric_list.append(seg_metrics)
+    # use_subprocess_isolation = len(img_files) > 1 and not options.get("debug")
+    # executor = ProcessPoolExecutor if use_subprocess_isolation else ThreadPoolExecutor
+    # print("Using", processes, "worker(s) with executor", executor.__name__)
+    # with executor(max_workers=processes) as executor:
+    #     futures = []
+    #     for img_file, mask_file, opt_img_file, cell_types in zip(
+    #         img_files, mask_files, opt_img_files, cell_types_by_image
+    #     ):
+    #         futures.append(
+    #             executor.submit(
+    #                 analysis,
+    #                 img_file,
+    #                 mask_file,
+    #                 opt_img_file,
+    #                 output_dir,
+    #                 options,
+    #                 cell_types,
+    #             )
+    #         )
+    #
+    #     for future in futures:
+    #         maybe_result = future.result()
+    #         if maybe_result is not None:
+    #             im, mask, cell_count, seg_metrics = maybe_result
+    #
+    #             im_list.append(im)
+    #             mask_list.append(mask)
+    #             cell_total.append(cell_count)
+    #             seg_metric_list.append(seg_metrics)
 
     ### CWL END ###
 
