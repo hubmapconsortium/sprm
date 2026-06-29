833c06fca104e6851f07a95f8697bfbb29d3d552
Sean <Sean.Donahue.5311@gmail.com>
2026-06-29 12:47:54 -0400

    Reading covariance matrix from CSV if for_spatialdata not available

--- sprm/SPRM_output_convert.py
+++ sprm/SPRM_output_convert.py
@@ -218,16 +218,16 @@ def read_table(sprm_dir: Path, expr_img_path: Path, spatialdata_dir: Path) -> Ta
         f"{expr_img_path.name}-nucleus_boundaries_channel_covar.npy",
     ]
 
-    if spatialdata_dir:
-        for covariance_matrix_path in covariance_matrix_paths:
-            a = np.ndarray((len(adata.var.index), len(adata.var.index), len(adata.obs.index)))
-            matrix = np.load(spatialdata_dir / covariance_matrix_path)
-            a = matrix.reshape(
-                (len(adata.obs.index), len(adata.var.index), len(adata.var.index))
-            ).transpose((1, 2, 0))
-            adata.varp[
-                covariance_matrix_path.replace(f"{expr_img_path.name}-", "").replace(".npy", "")
-            ] = a
+    for covariance_matrix_path in covariance_matrix_paths:
+        a = np.ndarray((len(adata.var.index), len(adata.var.index), len(adata.obs.index)))
+        matrix = np.load(spatialdata_dir / covariance_matrix_path) if covariance_matrix_path else pd.read_csv(sprm_dir\
+            / covariance_matrix_path.replace(".npy", ".csv")).drop("ID", axis=1, inplace=False).to_numpy()
+        a = matrix.reshape(
+            (len(adata.obs.index), len(adata.var.index), len(adata.var.index))
+        ).transpose((1, 2, 0))
+        adata.varp[
+            covariance_matrix_path.replace(f"{expr_img_path.name}-", "").replace(".npy", "")
+        ] = a
 
     adata.obs = sanitize_column_names(adata.obs)
     for column in adata.obs.columns:
